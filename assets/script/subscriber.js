ee6bb4088d1e6b1148cc24b2e3e4d3fb77112af4
Prabhjot <prabhjotk0187@gmail.com>
2026-03-26 21:51:40 -0500

    done with subscriber

--- assets/script/subscriber.js
+++ assets/script/subscriber.js
@@ -1,1 +1,24 @@
 'use strict';
+import { User } from "./user";
+
+export class Subscriber extends User {
+    #pages;
+    #groups;
+    #verified;
+
+    constructor(id, name, userName, email, pages, groups, verified) {
+    super(id, name, userName, email);
+    this.#pages = pages;
+    this.#groups = groups;
+    this.#verified = verified;
+    }
+
+    getInfo() {
+        return `
+      ${super.getInfo()}
+      <p>Pages: ${this.#pages.join(", ")}</p>
+      <p>Groups: ${this.#groups.join(", ")}</p>
+      <p>verified: ${this.#verified ? "Yes" : "No"}</p>
+    `;
+    }
+}
