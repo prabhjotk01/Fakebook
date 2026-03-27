3de9e85aed0ddabca88c2ca15d2b3e5be9fd7c64
Prabhjot <prabhjotk0187@gmail.com>
2026-03-26 22:48:21 -0500

    keep coding and editing main file

--- assets/script/app.js
+++ assets/script/app.js
@@ -21,96 +21,86 @@ const closeModal = document.getElementById("closeModal");
 const userInfo = document.getElementById("userInfo");
 const profilePic = document.querySelector(".profile-pic");
 
-    postText.addEventListener("input", () => {
-     if (postText.value.trim() !== "") {
-            postBtn.disabled = false;
+postText.addEventListener("input", () => {
+  if (postText.value.trim() !== "") {
+    postBtn.disabled = false;
   } else {
-         if (postImage.files.length > 0) {
+    if (postImage.files.length > 0) {
       postBtn.disabled = false;
-    }   else {
-            postBtn.disabled = true;
+    } else {
+      postBtn.disabled = true;
     }
   }
 });
 
-    postImage.addEventListener("change", () => {
-        if (postImage.files.length > 0) {
-            postBtn.disabled = false;
-  }     else {
-            if (postText.value.trim() !== "") {
-            postBtn.disabled = false;
-    }   else {
-            postBtn.disabled = true;
+postImage.addEventListener("change", () => {
+  if (postImage.files.length > 0) {
+    postBtn.disabled = false;
+  } else {
+    if (postText.value.trim() !== "") {
+      postBtn.disabled = false;
+    } else {
+      postBtn.disabled = true;
     }
   }
 });
 
+postBtn.addEventListener("click", () => {
+  let text = postText.value.trim();
+  let file = postImage.files[0];
 
-
-    postBtn.addEventListener("click", () => {
-
-    let text = postText.value.trim();
-    let file = postImage.files[0];
-
-  
-    if (text === "" && !file) {
-        return;
+  if (text === "" && !file) {
+    return;
   }
 
-    let div = document.createElement("div");
-        div.className = "post";
+  let div = document.createElement("div");
+  div.className = "post";
 
-    let date = new Date().toLocaleString();
+  let date = new Date().toLocaleString();
 
-    let img = "";
+  let img = "";
 
-        if (file) {
-            let url = URL.createObjectURL(file);
-        img = "<img src='" + url + "' class='post-img'>";
+  if (file) {
+    let url = URL.createObjectURL(file);
+    img = "<img src='" + url + "' class='post-img'>"; 
   }
 
-  
-    div.innerHTML = "";
+  div.innerHTML = "";
 
-    div.innerHTML += "<div class='post-header'>";
-    div.innerHTML += "<img src='assets/media/profile.png' class='profile-pic'>";
-    div.innerHTML += "<div>";
-    div.innerHTML += "<strong>" + user.name + "</strong><br>";
-    div.innerHTML += "<small>" + date + "</small>";
-    div.innerHTML += "</div>";
-    div.innerHTML += "</div>";
+  div.innerHTML += "<div class='post-header'>";
+  div.innerHTML += "<img src='assets/media/profile.png' class='profile-pic'>";
+  div.innerHTML += "<div>";
+  div.innerHTML += "<strong>" + user.name + "</strong><br>";
+  div.innerHTML += "<small>" + date + "</small>";
+  div.innerHTML += "</div>";
+  div.innerHTML += "</div>";
 
-        if (text !== "") {
-            div.innerHTML += "<p>" + text + "</p>";
+  if (text !== "") {
+    div.innerHTML += "<p>" + text + "</p>";
   }
 
-        if (img !== "") {
-            div.innerHTML += img;
+  if (img !== "") {
+    div.innerHTML += img;
   }
 
-        posts.prepend(div);
+  posts.prepend(div);
 
-  
-    postText.value = "";
-    postImage.value = "";
-    postBtn.disabled = true;
+  postText.value = "";
+  postImage.value = "";
+  postBtn.disabled = true;
 });
 
-
-
-    profilePic.addEventListener("click", () => {
-    modal.style.display = "block";
-    userInfo.innerHTML = user.getInfo();
+profilePic.addEventListener("click", () => {
+  modal.style.display = "block";
+  userInfo.innerHTML = user.getInfo();
 });
 
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-    closeModal.addEventListener("click", () => {
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
     modal.style.display = "none";
-});
-
-
-    window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-     modal.style.display = "none";
   }
 });
